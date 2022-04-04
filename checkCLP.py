18ae48591a4833bc34c252590df1fb141077a842
kamenoseiji <seiji.kameno@nao.ac.jp>
2022-04-04 14:14:38 -0400

    new file:   BS_uid___A002_Xf49cca_X23897_Scan6_SPW21-1.png 	new file:   checkCLP.py

---
+++ checkCLP.py
@@ -0,0 +1,7 @@
+SCR_DIR = '/users/skameno/ALMA_Py3/'
+wd = './'
+#---- DelayCal Scan
+spwList  = [21]
+BPscan = 6
+prefix = 'uid___A002_Xf49cca_X23897'
+exec(open(SCR_DIR + 'checkBispec.py').read())
