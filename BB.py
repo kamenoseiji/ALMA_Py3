81fd4f660e06ac2e9ceffeda17717726d86162a0
kamenoseiji <seiji.kameno@nao.ac.jp>
2023-10-06 16:57:08 +0000

    modified:   BB.py 	modified:   BB_scan.py 	modified:   BB_spw.py

--- BB.py
+++ BB.py
@@ -1,40 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from interferometry import indexList, GetAntName, GetPSpecScan, SPW2DATA_DESC_ID
 import datetime
-def GetAntName(msfile):
-    tb.open(msfile+'/'+'ANTENNA')
-    namelist = tb.getcol("NAME")
-    tb.close()
-    return namelist
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option('-u', dest='prefix', metavar='prefix',
+    help='EB UID   e.g. uid___A002_X10dadb6_X18e6', default='')
+parser.add_option('-s', dest='spw', metavar='spw',
+    help='SPW ID   e.g. 13', default='13')
+parser.add_option('-c', dest='scanID', metavar='scanID',
+    help='Scan ID  e.g. 2', default='2')
 #
-def GetPSpecScan(msfile, ant, spwID, scanID):
-    data_desc_id = SPW2DATA_DESC_ID(msfile, spwID)
-    Out='ANTENNA1 == %d && ANTENNA2 == %d && DATA_DESC_ID == %d && SCAN_NUMBER == %d' % (ant, ant, data_desc_id, scanID)
-    tb.open(msfile)
-    antXantYspw = tb.query(Out)
-    colNameList = antXantYspw.colnames()
-    colName = 'DATA'
-    if 'FLOAT_DATA' in colNameList: colName = 'FLOAT_DATA'
-    timeXY = antXantYspw.getcol('TIME')
-    dataXY = antXantYspw.getcol(colName)
-    tb.close()
-    return timeXY, dataXY.real
-#
-def SPW2DATA_DESC_ID(msfile, spwID):
-    tb.open(msfile + '/' + 'DATA_DESCRIPTION')
-    data_desc_id, spw_index = -1,-1
-    while( spw_index !=  spwID ):
-        data_desc_id += 1
-        spw_index = tb.getcell('SPECTRAL_WINDOW_ID', data_desc_id)
-    tb.close()
-    return data_desc_id
+(options, args) = parser.parse_args()
+prefix  = options.prefix.replace("/", "_").replace(":","_").replace(" ","")
+scanID  = int(options.scanID)
+spwID   = int(options.spw)
+#-------- Functions
+def medianATM(subscanDic, power):    # clustering sky/amb/hot
+    for subScan in subscanDic.keys():
+        if len(subscanDic[subScan]) < 3:
+            subscanDic[subScan] = [np.nan, np.nan]
+        else:
+            scanPower = [ np.median(power[0, subscanDic[subScan]]), np.median(power[1, subscanDic[subScan]]) ]
+            subscanDic[subScan] = scanPower
+        #
+    #
+    return subscanDic
 #
 #-------- Parameters
-prefix = 'uid___A002_Xff81e8_X253'
 msfile = prefix + '.ms'
-scanID = 9
-spwID = 0
+msmd.open(msfile)
+timeOFF, timeON, timeAMB, timeHOT, timeTEST = msmd.timesforintent("CALIBRATE_ATMOSPHERE#OFF_SOURCE"), msmd.timesforintent("CALIBRATE_ATMOSPHERE#ON_SOURCE"), msmd.timesforintent("CALIBRATE_ATMOSPHERE#AMBIENT"), msmd.timesforintent("CALIBRATE_ATMOSPHERE#HOT"), msmd.timesforintent("CALIBRATE_ATMOSPHERE#TEST")
+subscanDic = dict( zip(['OFF', 'ON', 'AMB', 'HOT', 'TEST'], [[]]* 5))
 #-------- Main process
 DT = []
 antList = GetAntName(msfile)
@@ -47,13 +45,34 @@ for ant_index, antName in enumerate(antList):
     if len(DT) == 0:
         for mjdSec in timeStamp.tolist(): DT.append(datetime.datetime.strptime(qa.time('%fs' % (mjdSec), form='fits', prec=9)[0], '%Y-%m-%dT%H:%M:%S.%f'))
     #
+    #-------- Subscan Intents
+    subscanDic['OFF'], subscanDic['ON'],subscanDic['AMB'],subscanDic['HOT'],subscanDic['TEST'] = indexList(timeOFF, timeStamp), indexList(timeON, timeStamp), indexList(timeAMB, timeStamp), indexList(timeHOT, timeStamp), indexList(timeTEST, timeStamp)
+    subscanDic = medianATM(subscanDic, BBPower[:,0])
+    text_X, text_Y = 'Pol-X ', 'Pol-Y '
+    for subScan in subscanDic.keys():
+        if subscanDic[subScan] == [np.nan, np.nan] : continue
+        text_X = text_X + subScan + ':'
+        text_Y = text_Y + subScan + ':'
+    #
+    text_X, text_Y = text_X[:len(text_X)-1] + '=', text_Y[:len(text_Y)-1] + '='
+    for subScan in subscanDic.keys():
+        if subscanDic[subScan] == [np.nan, np.nan] : continue
+        if subScan == 'AMB':
+            text_X = text_X + '1:'
+            text_Y = text_Y + '1:'
+        else:
+            text_X = text_X + '%.3f:' % (subscanDic[subScan][0] / subscanDic['AMB'][0])
+            text_Y = text_Y + '%.3f:' % (subscanDic[subScan][1] / subscanDic['AMB'][1])
+        #
+    text_X, text_Y = text_X[:len(text_X)-1], text_Y[:len(text_Y)-1]
+    #-------- Plot
     BBPL = figBB.add_subplot( int((antNum + 3)/4), 4, ant_index + 1)
-    BBPL.step( DT, BBPower[0,0], where='mid'); BBPL.plot( DT, BBPower[0,0], 'o', label=antName + 'Pol X')
-    BBPL.step( DT, BBPower[1,0], where='mid'); BBPL.plot( DT, BBPower[1,0], 'o', label=antName + 'Pol Y')
-    BBPL.legend(loc='lower right', prop={'size' :7}, numpoints=1)
-    BBPL.tick_params(axis='both', labelsize=6)
+    BBPL.step( DT, BBPower[0,0], where='mid'); BBPL.plot( DT, BBPower[0,0], 'b.', label=text_X)
+    BBPL.step( DT, BBPower[1,0], where='mid'); BBPL.plot( DT, BBPower[1,0], 'g.', label=text_Y)
+    BBPL.legend(loc='best', prop={'size' :3}, numpoints=1)
+    BBPL.set_title(antName, fontsize=5, pad=-14)
+    BBPL.tick_params(axis='both', labelsize=4)
 #
 figBB.savefig(pp, format='pdf')
 plt.close('all')
 pp.close()
-
--- BB_scan.py
+++ BB_scan.py
@@ -1,61 +1,45 @@
-#-------- BB_spw : BB power measurements for list of spws, single antenna, single scan
-prefix = 'uid___A002_X106ccd1_Xe59d'
-antName = 'DV20'
-spwID = 13
-scanList = [2 , 4 , 6 , 13, 16]
-#-------- Functions
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from interferometry import indexList, GetAntName, GetPSpecScan, SPW2DATA_DESC_ID
 import datetime
-def GetAntName(msfile):
-    tb.open(msfile+'/'+'ANTENNA')
-    namelist = tb.getcol("NAME")
-    tb.close()
-    return namelist
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option('-u', dest='prefix', metavar='prefix',
+    help='EB UID   e.g. uid___A002_X10dadb6_X18e6', default='')
+parser.add_option('-a', dest='antName', metavar='antName',
+    help='Antenna name', default='')
+parser.add_option('-c', dest='scanList', metavar='scanList',
+    help='Scan List e.g. 2, 5, 10', default='')
+parser.add_option('-s', dest='spwID', metavar='spwID',
+    help='SPW ID   e.g. 13', default='13')
 #
-def GetPSpecScan(msfile, ant, spwID, scanID):
-    data_desc_id = SPW2DATA_DESC_ID(msfile, spwID)
-    Out='ANTENNA1 == %d && ANTENNA2 == %d && DATA_DESC_ID == %d && SCAN_NUMBER == %d' % (ant, ant, data_desc_id, scanID)
-    tb.open(msfile)
-    antXantYspw = tb.query(Out)
-    colNameList = antXantYspw.colnames()
-    colName = 'DATA'
-    if 'FLOAT_DATA' in colNameList: colName = 'FLOAT_DATA'
-    timeXY = antXantYspw.getcol('TIME')
-    dataXY = antXantYspw.getcol(colName)
-    tb.close()
-    return timeXY, dataXY.real
-#
-def SPW2DATA_DESC_ID(msfile, spwID):
-    tb.open(msfile + '/' + 'DATA_DESCRIPTION')
-    data_desc_id, spw_index = -1,-1
-    while( spw_index !=  spwID ):
-        data_desc_id += 1
-        spw_index = tb.getcell('SPECTRAL_WINDOW_ID', data_desc_id)
-    tb.close()
-    return data_desc_id
-#
-def kmedianATM(power):    # clustering sky/amb/hot
-    num_sample = len(power)
-    init_index = [list(range(int(num_sample/3))), list(range(int(num_sample/3), int(2*num_sample/3))), list(range(int(2*num_sample/3), num_sample))]
-    prev_index = init_index
-    while True:
-        new_index = [[],[],[]]
-        prev_center = [np.median(power[prev_index[0]]), np.median(power[prev_index[1]]), np.median(power[prev_index[2]]) ]
-        for index in list(range(num_sample)):
-            nearest_index = np.argmin( abs( power[index] - np.array(prev_center) ) )
-            new_index[nearest_index] = new_index[nearest_index] + [index]
+(options, args) = parser.parse_args()
+#-------- BB_spw : BB power measurements for list of spws, single antenna, single scan
+prefix  = options.prefix.replace("/", "_").replace(":","_").replace(" ","")
+antName = options.antName
+spwID  =  int(options.spwID)
+scanList = options.scanList.split(',')
+scanList = [int(scan) for scan in scanList]
+scanNum  = len(scanList)
+#-------- Functions
+def medianATM(subscanDic, power):    # clustering sky/amb/hot
+    for subScan in subscanDic.keys():
+        if len(subscanDic[subScan]) < 3:
+            subscanDic[subScan] = [np.nan, np.nan]
+        else:
+            scanPower = [ np.median(power[0, subscanDic[subScan]]), np.median(power[1, subscanDic[subScan]]) ]
+            subscanDic[subScan] = scanPower
         #
-        new_center = [np.median(power[new_index[0]]), np.median(power[new_index[1]]), np.median(power[new_index[2]]) ]
-        if new_center == prev_center: break
-        prev_index = new_index
     #
-    return np.array(new_center)
+    return subscanDic
 #
 #-------- Main process
 msfile = prefix + '.ms'
+msmd.open(msfile)
+timeOFF, timeON, timeAMB, timeHOT, timeTEST = msmd.timesforintent("CALIBRATE_ATMOSPHERE#OFF_SOURCE"), msmd.timesforintent("CALIBRATE_ATMOSPHERE#ON_SOURCE"), msmd.timesforintent("CALIBRATE_ATMOSPHERE#AMBIENT"), msmd.timesforintent("CALIBRATE_ATMOSPHERE#HOT"), msmd.timesforintent("CALIBRATE_ATMOSPHERE#TEST")
+subscanDic = dict( zip(['OFF', 'ON', 'AMB', 'HOT', 'TEST'], [[]]* 5))
 antList = GetAntName(msfile)
 ant_index = np.where(antList == antName)[0][0]
 pp = PdfPages('SQLD_%s_%s_SPW%d.pdf' % (prefix, antName, spwID))
@@ -64,24 +48,39 @@ figBB.suptitle('%s %s SQLD Power SPW=%d' % (prefix, antName, spwID))
 scanNum = len(scanList)
 panelNum_X = int(math.sqrt(scanNum + 1))
 panelNum_Y = math.ceil( scanNum / panelNum_X )
+DT = []
 for scan_index, scanID in enumerate(scanList):
     timeStamp, BBPower = GetPSpecScan(msfile, ant_index, spwID, scanID)
-    medianPowerX, medianPowerY = kmedianATM(BBPower[0,0]), kmedianATM(BBPower[1,0])
-    text_X = 'Pol-X Po:Pa:Ph=%.3f:1:%.3f' % ( medianPowerX[0]/medianPowerX[1],medianPowerX[2]/medianPowerX[1] )
-    text_Y = 'Pol-Y Po:Pa:Ph=%.3f:1:%.3f' % ( medianPowerY[0]/medianPowerY[1],medianPowerY[2]/medianPowerY[1] )
-    DT = []
-    for mjdSec in timeStamp.tolist(): DT.append(datetime.datetime.strptime(qa.time('%fs' % (mjdSec), form='fits', prec=9)[0], '%Y-%m-%dT%H:%M:%S.%f'))
+    subscanDic['OFF'], subscanDic['ON'],subscanDic['AMB'],subscanDic['HOT'],subscanDic['TEST'] = indexList(timeOFF, timeStamp), indexList(timeON, timeStamp), indexList(timeAMB, timeStamp), indexList(timeHOT, timeStamp), indexList(timeTEST, timeStamp)
+    subscanDic = medianATM(subscanDic, BBPower[:,0])
+    text_X, text_Y = 'Pol-X ', 'Pol-Y '
+    for subScan in subscanDic.keys():
+        if subscanDic[subScan] == [np.nan, np.nan] : continue
+        text_X = text_X + subScan + ':'
+        text_Y = text_Y + subScan + ':'
+    #
+    text_X, text_Y = text_X[:len(text_X)-1] + '=', text_Y[:len(text_Y)-1] + '='
+    for subScan in subscanDic.keys():
+        if subscanDic[subScan] == [np.nan, np.nan] : continue
+        if subScan == 'AMB':
+            text_X = text_X + '1:'
+            text_Y = text_Y + '1:'
+        else:
+            text_X = text_X + '%.3f:' % (subscanDic[subScan][0] / subscanDic['AMB'][0])
+            text_Y = text_Y + '%.3f:' % (subscanDic[subScan][1] / subscanDic['AMB'][1])
+        #
+    text_X, text_Y = text_X[:len(text_X)-1], text_Y[:len(text_Y)-1]
+    if len(DT) != len(timeStamp):
+        DT = []
+        for mjdSec in timeStamp.tolist(): DT.append(datetime.datetime.strptime(qa.time('%fs' % (mjdSec), form='fits', prec=9)[0], '%Y-%m-%dT%H:%M:%S.%f'))
     #
     BBPL = figBB.add_subplot( panelNum_Y, panelNum_X, scan_index + 1)
     BBPL.step( DT, BBPower[0,0], where='mid'); BBPL.plot( DT, BBPower[0,0], 'b.', label=text_X)
     BBPL.step( DT, BBPower[1,0], where='mid'); BBPL.plot( DT, BBPower[1,0], 'g.', label=text_Y)
-    BBPL.legend(loc='lower right', prop={'size' :7}, numpoints=1)
-    BBPL.tick_params(axis='both', labelsize=6)
+    BBPL.legend(loc='best', prop={'size' :5}, numpoints=1)
+    BBPL.tick_params(axis='both', labelsize=4)
     BBPL.set_title('SCAN %d' % scanID)
 #
 figBB.savefig(pp, format='pdf')
 plt.close('all')
 pp.close()
-
-
-
