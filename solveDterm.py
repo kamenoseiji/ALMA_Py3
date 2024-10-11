21ff955be40da638dfca54db46f37b4f98ca049d
kamenoseiji <seiji.kameno@nao.ac.jp>
2024-10-11 19:27:59 +0000

    modified:   checkPolCalScans.py 	modified:   solveDterm.py

--- solveDterm.py
+++ solveDterm.py
@@ -1,4 +1,4 @@
-SCR_DIR = '/home/skameno/ALMA_Py3/'
+SCR_DIR = '/users/skameno/ALMA_Py3/'
 R_DIR = '/bin/'
 import sys
 import math
@@ -28,11 +28,11 @@ antFlag = [ant for ant in options.antFlag.split(',')]
 spwList = [int(spw) for spw in options.spwList.split(',')]
 scanList = [int(scan) for scan in options.scanList.split(',')]
 '''
-prefix = 'BL_X11e3e46_X4b5d'
+prefix = '2023.1.01268.S_X1104ab5_X13143'
 refant = 'CM03'
 antFlag = []
 spwList = [0,1,2,3]
-scanList =[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85]
+scanList =[3,6,9,31,52,57]
 '''
 #----------------------------------------- Procedures
 def flagOutLier(value, thresh=5.0):
