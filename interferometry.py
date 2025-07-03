2b23756300397d6e8292d0e68a9c903e5e8bc9a8
kamenoseiji <seiji.kameno@alma.cl>
2025-07-03 18:10:01 +0000

    modified:   interferometry.py

--- interferometry.py
+++ interferometry.py
@@ -1377,7 +1377,7 @@ def delay_search( spec ):
     fit = np.polyfit(trial_delay, trial_amp, 2)
     bestDelay = -fit[1]/(2.0*fit[0])
     residualPhase = np.angle( delay_cal(spec, bestDelay) )
-    snr = 32.0 / (AllanVarPhase(residualPhase, int(nspec/4))* nspec* nspec + 1.0e-6)
+    snr = 0.25 / (AllanVarPhase(residualPhase, int(nspec/4))* nspec + 1.0e-9)
     return bestDelay, snr
 #
 def blGain( blSpec ):				# Average in spectral channels
