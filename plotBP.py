204daa73846710b56deded9e9a5371bb90dbc434
kamenoseiji <seiji.kameno@alma.cl>
2025-09-10 22:14:57 +0000

    modified:   plotBP.py 	new file:   plotSPWphase.py

--- plotBP.py
+++ plotBP.py
@@ -15,37 +15,44 @@ parser.add_option('-p', dest='plotAnt', metavar='plotAnt',
     help='Antennas to plot', default='')
 parser.add_option('-s', dest='spwList', metavar='spwList',
     help='SPWs to plot e.g. 17,19,21', default='')
+parser.add_option('-R', dest='refant', metavar='refant',
+    help='Reference antenna e.g. DA41', default='')
 (options, args) = parser.parse_args()
 prefix  = options.prefix.replace("/", "_").replace(":","_").replace(" ","")
 scanList= [int(scan) for scan in options.scanList.split(',')]
 spwList = [int(spw) for spw in options.spwList.split(',')]
 plotAntList = [] if options.plotAnt == '' else [ant for ant in options.plotAnt.split(',')]
+refantName = options.refant
+'''
+prefix = 'uid___A002_X12dca4b_Xcf44'
+scanList = [3,5,7]
+spwList = [5,17,21,23]
+plotAntList = ['DA41','DA42']
+refantName = 'DA51'
+'''
 antList = np.load(prefix + '.Ant.npy')
 if plotAntList == []: plotAntList = antList
 antMap = indexList(plotAntList, antList)
 plotAntList = antList[antMap].tolist()
 plotAntNum, columnNum = len(plotAntList), len(spwList)
-#exec(open(SCR_DIR + 'interferometry.py').read())
-#exec(open(SCR_DIR + 'Plotters.py').read())
-#
 #-------- Bandpass Table
 antList = np.load('%s-REF%s.Ant.npy' % (prefix, refantName))
 BPList, FreqList = [], []
-#spwNum, BPscanNum, scanNum  = len(spwList), len(BPscanList), len(scanList)
 for spw_index, spw in enumerate(spwList):
-    Freq   = np.load('%s-SPW%d-Freq.npy' % (prefix, spw))
     #-- First Target scan
     for scan_index, scan in enumerate(scanList):
+        print('--Loading Bandpass Table for spw%d scan%d' % (spw, scan))
         BPList = BPList + [np.load('%s-REF%s-SC%d-SPW%d-BPant.npy' % (prefix, refantName, scan, spw))]
+        FreqList = FreqList + [np.load('%s-SPW%d-Freq.npy' % (prefix, spw))]
     #
-    BPList, FreqList = BPList + [BP_ant], FreqList + [Freq]
 #
-ppolNum = BP_ant.shape[1]
+ppolNum = BPList[0].shape[1]
 PolList = ['X', 'Y']
-#
 #-------- Plots
-pp = PdfPages('BP_%s_REF%s_Scan%d.pdf' % (prefix, refantName, BPscan))
 if 'plotMin' not in locals(): plotMin = 0.0
 if 'plotMax' not in locals(): plotMax = 1.2
-plotSP(pp, prefix, antList, spwList, FreqList, BPList, plotMin, plotMax) 
+for scan_index, scan in enumerate(scanList):
+    print('--Plotting scan%d' % (scan))
+    pp = PdfPages('BP_%s_REF%s_Scan%d.pdf' % (prefix, refantName, scan))
+    plotSP(pp, prefix, antList, spwList, FreqList, BPList, plotMin, plotMax) 
 #
---
+++ plotSPWphase.py
@@ -0,0 +1,60 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ptick
+import matplotlib.cm as cm
+from matplotlib.backends.backend_pdf import PdfPages
+from casatools import quanta as qatool
+import analysisUtils as au
+import datetime
+from interferometry import indexList, RADDEG
+from Plotters import polColor, polName
+qa = qatool()
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option('-u', dest='prefix', metavar='prefix',
+    help='EB UID   e.g. uid___A002_X10dadb6_X18e6', default='')
+parser.add_option('-p', dest='plotAnt', metavar='plotAnt',
+    help='Antennas to plot', default='')
+parser.add_option('-s', dest='spwList', metavar='spwList',
+    help='SPWs to plot e.g. 17,19,21', default='')
+(options, args) = parser.parse_args()
+prefix  = options.prefix.replace("/", "_").replace(":","_").replace(" ","")
+spwList = [int(spw) for spw in options.spwList.split(',')]
+plotAntList = [] if options.plotAnt == '' else [ant for ant in options.plotAnt.split(',')]
+antList = np.load(prefix + '.Ant.npy')
+if plotAntList == []: plotAntList = antList
+antMap = indexList(plotAntList, antList)
+plotAntList = antList[antMap].tolist()
+plotAntNum, columnNum = len(plotAntList), len(spwList)
+#-------- Load tables
+polLabel = ['X', 'Y']
+polSymbol = ['x', '1']
+cmap = plt.get_cmap("tab10")
+pp = PdfPages('GA_%s.pdf' %  (prefix))
+figPhs = plt.figure(figsize = (8, 11))
+figPhs.suptitle(prefix + ' Gain Phase')
+for row_index, ant in enumerate(plotAntList):
+    ant_index = np.where(antList == ant)[0][0]
+    timeFile = '%s-SPW%d.TS.npy' % (prefix, spwList[0])
+    timeStamp = np.load(timeFile)
+    DT = [datetime.datetime.strptime(au.call_qa_time('%fs' % (mjdSec), form='fits', prec=9), '%Y-%m-%dT%H:%M:%S.%f') for mjdSec in timeStamp.tolist()]
+    for pol_index, polchar in enumerate(polSymbol):
+        PhsPL = figPhs.add_subplot( plotAntNum, 2, 2*row_index + pol_index + 1 )
+        PhsPL.yaxis.offsetText.set_fontsize(3)
+        PhsPL.tick_params(labelsize=4); PhsPL.tick_params(axis='x', rotation=45)
+        PhsPL.axis([np.min(DT), np.max(DT), -60.0, 60.0])
+        PhsPL.set_ylabel(ant, fontsize=6)
+        PhsPL.set_xticks([])
+        for spw_index, spw in enumerate(spwList):
+            Gain, FG = np.load('%s-SPW%d.GA.npy' % (prefix, spw)), np.load('%s-SPW%d.FG.npy' % (prefix, spw))
+            flag_index = np.where(FG[ant_index] > 0.01)[0].tolist()
+            if spw_index == 0: GA0 = Gain[ant_index, pol_index, flag_index]*Gain[ant_index, pol_index, 0].conjugate()
+            PhsPL.plot( np.array(DT)[flag_index], np.angle(Gain[ant_index, pol_index, flag_index]*Gain[ant_index, pol_index, 0].conjugate() / GA0)*RADDEG, polchar, markersize=2, color=cmap(spw_index), label='SPW%d Pol%s' % (spw, polLabel[pol_index]))
+            #PhsPL.plot( np.array(DT)[flag_index], np.angle(Gain[ant_index, pol_index, flag_index]*Gain[ant_index, pol_index, 0].conjugate())*RADDEG, polchar, markersize=2, color=cmap(spw_index), label='SPW%d Pol%s' % (spw, polLabel[pol_index]))
+        if row_index == 0: PhsPL.legend(loc = 'best', prop={'size' :4}, numpoints = 1)
+    #
+figPhs.text(0.1, 0.03, 'See https://github.com/kamenoseiji/ALMA_Py3/wiki/plotSPWphase.py to generate this plot', fontsize=4)
+figPhs.savefig(pp, format='pdf')
+plt.close('all')
+pp.close()
