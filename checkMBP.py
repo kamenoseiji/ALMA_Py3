0c6f2fc3557657195a74e27017b464d0bc86f470
kamenoseiji <seiji.kameno@alma.cl>
2025-10-21 20:06:25 +0000

    modified:   checkMBP.py

--- checkMBP.py
+++ checkMBP.py
@@ -5,11 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ptick
 from matplotlib.backends.backend_pdf import PdfPages
-from interferometry import GetBaselineIndex, CrossCorrAntList, Ant2Bl, Ant2BlD, Bl2Ant, indexList, ANT0, ANT1, bestRefant, bunchVec, GetAntName, GetUVW, GetChNum, BPtable, GetVisAllBL, gainComplexVec, ParaPolBL
+from interferometry import GetBaselineIndex, CrossCorrAntList, Ant2Bl, Ant2BlD, Bl2Ant, indexList, ANT0, ANT1, bestRefant, bunchVec, GetAntName, GetUVW, GetChNum, BPtable, GetVisAllBL, gainComplexVec, ParaPolBL, RADDEG
 from ASDM_XML import SPW_FULL_RES, spwMS, spwIDMS
-from Plotters import plotXYP, plotBP, plotSP
+from Plotters import plotXYP, plotBP, plotSP, polColor, polColor
 from optparse import OptionParser
-'''
 parser = OptionParser()
 parser.add_option('-u', dest='prefix', metavar='prefix',
     help='EB UID   e.g. uid___A002_X10dadb6_X18e6', default='')
@@ -23,12 +22,14 @@ parser.add_option('-c', dest='scanList', metavar='scanList',
     help='Scan List  e.g. 3,6', default='')
 parser.add_option('-f', dest='FG', metavar='FG',
     help='Apply flagging', action="store_true")
+parser.add_option('-g', dest='GAspw', metavar='GAspw',
+    help='SPW for gain table to apply', default='')
 parser.add_option('-m', dest='plotMin', metavar='plotMin', type="float",
     help='Plot range minimum', default=0.0)
 parser.add_option('-M', dest='plotMax', metavar='plotMax', type="float",
     help='Plot range minimum', default=1.2)
-parser.add_option('-n', dest='NPY', metavar='NPY',
-    help='Generate numpy files (.npy)', action="store_true")
+parser.add_option('-r', dest='refscan', metavar='refscan',
+    help='Reference (bandpass) scan', default='3')
 parser.add_option('-s', dest='spwList', metavar='spwList',
     help='SPW List e.g. 0,1,2,3', default='')
 parser.add_option('-P', dest='BPPLOT', metavar='BPPLOT',
@@ -58,18 +59,21 @@ plotMax = options.plotMax
 BPPLOT  = options.BPPLOT
 delayMessage  = options.delayMessage
 FG      = options.FG
-NPY     = options.NPY
 XYLog   = options.XYLog
 refant  = options.refant
+GAspw   = int(options.GAspw)
+GAprefix = BPprefix
+refScan = int(options.refscan)
 '''
-BPprefix = 'uid___A002_X12e95ca_X7383'
-GAprefix = 'uid___A002_X12e95ca_X7383'
-prefix = 'uid___A002_X12e95ca_X7383'
-antFlag = ['DA50','DV24']
+BPprefix = 'uid___A002_X12e95ca_X7383.WVR'
+GAprefix = 'uid___A002_X12e95ca_X7383.WVR'
+prefix = 'uid___A002_X12e95ca_X7383.WVR'
+antFlag = []
 refScan = 3
-scanList = [28]
-spwList = [15,17,19,21]
-GAspw   = 15
+scanList = [26]
+#spwList = [15,17,19,21]
+spwList = [0,2,4,6]
+GAspw   = 0
 chBin = 1
 plotMin = 0.0
 plotMax = 1.5
@@ -77,6 +81,21 @@ XYLog = False
 BPPLOT = True
 FG     = False
 refant = 'DA45'
+'''
+#--------
+def delayFit(freq, spec):
+    chNum = len(freq)
+    normspec = np.array((spec.real/abs(spec)).tolist() +  (spec.imag/abs(spec)).tolist())
+    refFreq  = np.array((freq - np.median(freq)).tolist() + (freq - np.median(freq)).tolist())                     # Frequency relative to the center
+    param = np.array([np.median(np.angle(spec)),0.0 ])  # initial phase and delay
+    P = np.array([(-normspec[chNum:2*chNum]).tolist() + normspec[0:chNum].tolist(), (-normspec[chNum:2*chNum]).tolist() + normspec[0:chNum].tolist()])
+    P[1] = P[1]* 2.0* np.pi* refFreq
+    for iteration in list(range(3)):
+        resid = np.array((normspec[0:chNum] - np.cos( param[0] + 2.0* np.pi* param[1]* refFreq[0:chNum])).tolist() + (normspec[chNum:2*chNum] - np.sin( param[0] + 2.0* np.pi* param[1]* refFreq[chNum:2*chNum])).tolist())
+        correction = np.linalg.inv(P.dot(P.T)).dot(P.dot(resid))
+        param = param + correction
+    return param
+#
 #-------- bandpass and gain tables to apply
 msfile = prefix + '.ms'
 BPantList = np.load('%s-REF%s.Ant.npy' % (BPprefix, refant))
@@ -114,55 +133,60 @@ ant0 = ANT0[0:UseBlNum]; ant1 = ANT1[0:UseBlNum]
 for bl_index in list(range(UseBlNum)): blMap[bl_index], blInv[bl_index]  = Ant2BlD(antMap[ant0[bl_index]], antMap[ant1[bl_index]])
 print( '  %d baselines are inverted.' % (len(np.where( blInv )[0])))
 #-------- Gain Table
-GA = np.load(GAfileName)[antMap]
+GA = np.load(GAfileName)
 TS = np.load(TSfileName)
 #-------- Bandpass Table
 print('---Loading antenna-based bandpass table')
 SideBand = ['LSB', 'USB']
-FreqList, BPList = [], []
+FreqList, BPList, Freq1D = [], [], []
 for spw_index, spw in enumerate(spwList):
     BPList   = BPList + [np.load(BPfileList[spw_index])]
     FreqList = FreqList + [np.load(BPfreqList[spw_index])]
+    SPWdic[spw]['chRange'] = list(range(int(0.05*SPWdic[spw]['chNum']), int(0.95*SPWdic[spw]['chNum'])))
+    Freq1D = Freq1D + np.load(BPfreqList[spw_index])[SPWdic[spw]['chRange']].tolist()
+Freq1D = 1.0e-9* np.array(Freq1D)
 #-------- Load Visiblities
-print('---Loading visibilities')
-for scan_index, scan in scanList:
+GDL = np.zeros([4* antNum + 1,len(scanList)])
+for scan_index, scan in enumerate(scanList):
+    caledBPList = []
+    pp = PdfPages('PHS_%s_REF%s_Scan%d.pdf' % (prefix, BPantList[0], scan))
+    figAnt = plt.figure(figsize = (11, 8))
+    figAnt.suptitle('%s Scan %d Antenna-based phase' % (prefix, scan))
+    figAnt.text(0.45, 0.05, 'Frequency [GHz]')
+    figAnt.text(0.03, 0.45, 'Phase [deg]', rotation=90)
     for spw_index, spw in enumerate(spwList):
+        print('---Loading visibilities Scan %d SPW %d' % (scan, spw))
         timeStamp, Pspec, Xspec = GetVisAllBL(msfile, spw, scan)    # Xspec[pol, ch, bl, time]
-        Xspec  = ParaPolBL(Xspec[:,:,blMap], blInv)
         GAscan = GA[:,:,indexList(timeStamp, TS)]                   # GAscan[ant, pol, time]
+        Xspec  = ParaPolBL(Xspec[:,:,blMap], blInv)
         BPant  = BPList[spw_index][BPantMap]                        # BPant[ant, pol, ch]
         CaledXspec = np.mean(GAscan[ant0].conjugate()* GAscan[ant1]* Xspec.transpose(1,2,0,3), axis=3).transpose(1,2,0) # CaledXspec[bl, pol, ch]
         CaledXspec = CaledXspec / (BPant[ant0]* BPant[ant1].conjugate())
-'''
-
-
-
-        BP_ant[:,0], BP_ant[:,1] = (gainComplexVec(CaledXspec[:,:,0].T), gainComplexVec(CaledXspec[:,:,1].T))
+        caledBPList = caledBPList + [np.array([gainComplexVec(CaledXspec[:,0]), gainComplexVec(CaledXspec[:,1])]).transpose(1,0,2)]
+    GDL[0, scan_index] = np.median(timeStamp)
+    for ant_index, ant in enumerate(antList[antMap]):
+        PhsPL = figAnt.add_subplot(1, 1, 1 )
+        PhsPL.set_title(ant)
+        BPX, BPY = [], []
+        for spw_index, spw in enumerate(spwList):
+            BPX = BPX + caledBPList[spw_index][ant_index,0][SPWdic[spw]['chRange']].tolist()
+            BPY = BPY + caledBPList[spw_index][ant_index,1][SPWdic[spw]['chRange']].tolist()
+        BPX, BPY = np.array(BPX), np.array(BPY)
+        paramX, paramY = delayFit(Freq1D, BPX), delayFit(Freq1D, BPY)
+        GDL[1 +            ant_index, scan_index] = paramX[0]
+        GDL[1 +   antNum + ant_index, scan_index] = paramY[0]
+        GDL[1 + 2*antNum + ant_index, scan_index] = paramX[1]
+        GDL[1 + 3*antNum + ant_index, scan_index] = paramY[1]
+        PhsPL.plot(Freq1D, RADDEG* np.angle(BPX), '.', color=polColor[0], label = 'Pol-%s DL=%+.3f [ps] phs=%.1f [deg]' % ('X', 1e3*paramX[1], RADDEG*paramX[0]))
+        PhsPL.plot(Freq1D, RADDEG* np.angle(BPY), '.', color=polColor[1], label = 'Pol-%s DL=%+.3f [ps] phs=%.1f [deg]' % ('Y', 1e3*paramY[1], RADDEG*paramY[0]))
+        text_sd = 'Scan%d %s : multi-delay = %6.3f %6.3f [ps]  %6.1f %6.1f [deg]' % (scan, ant, 1e3*paramX[1], 1e3*paramY[1], RADDEG*paramX[0], RADDEG*paramY[0])
+        print(text_sd)
+        PhsPL.axis([np.min(Freq1D), np.max(Freq1D), -30.0, 30.0])
+        PhsPL.tick_params(axis='both', labelsize=9)
+        PhsPL.legend(loc = 'lower left', prop={'size' :7}, numpoints = 1)
+        figAnt.savefig(pp, format='pdf')
+        figAnt.delaxes(PhsPL)
     #
-    ppolNum = BPList[0].shape[1]
-    PolList = ['X', 'Y']
-    #
-    #-------- Save CalTables
-    if NPY:
-        np.save(prefix + '-REF' + antList[UseAnt[refantID]] + '.Ant.npy', antList[antMap]) 
-        for spw_index, spw in enumerate(spws):
-            np.save('%s-REF%s-SC%d-SPW%d-BPant.npy' % (prefix, antList[UseAnt[refantID]], BPscan, spw), BPList[spw_index]) 
-            np.save('%s-REF%s-SC%d-SPW%d-XYspec.npy' % (prefix, antList[UseAnt[refantID]], BPscan, spw), XYList[spw_index]) 
-            np.save('%s-REF%s-SC%d-SPW%d-XYdelay.npy' % (prefix, antList[UseAnt[refantID]], BPscan, spw), XYdelayList[spw_index]) 
-    #
-    #-------- Plots
-    if BPPLOT:
-        if XYsnr > 0.0:
-            pp = PdfPages('XYP_%s_REF%s_Scan%d.pdf' % (prefix, antList[UseAnt[refantID]], BPscan))
-            plotXYP(pp, prefix, spws, XYList, XYdelayList, chBin) 
-        #
-        pp = PdfPages('BP_%s_REF%s_Scan%d.pdf' % (prefix, antList[UseAnt[refantID]], BPscan))
-        if 'spurRFLists' in locals():
-            delay_ant = plotBP(pp, prefix, antList[antMap], spws, BPscan, BPList, chBin, 1.2, spurRFLists) 
-        else:
-            delay_ant = plotSP(pp, prefix, antList[antMap], spws, FreqList, BPList, plotMin, plotMax, delayMessage)
-        if NPY:
-            for spw_index, spw in enumerate(spws): np.save('%s-REF%s-SC%d-SPW%d-DL.npy' % (prefix, antList[UseAnt[refantID]], BPscan, spw), delay_ant[:,spw_index])
-    #
-if XYLog: xyLog.close()
-'''
+    plt.close('all')
+    pp.close()
+np.save('%s-GDL.npy' % (prefix), GDL)
