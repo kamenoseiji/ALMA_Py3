c3380ee5e320764a249863262007898c9ab8136a
Seiji.Kameno <seiji.kameno@nao.ac.jp>
2026-06-16 15:44:58 -0400

    new file:   SLTquery.py 	deleted:    hidoi.tex

---
+++ SLTquery.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import datetime
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option('-s', dest='SBcode', metavar='SBcode',
+    help='SB code  e.g. PSO_Grid', default='PSO_Grid')
+parser.add_option('-d', dest='days', metavar='days',
+    help='Backword days', default='4')
+(options, args) = parser.parse_args()
+backDays=  int(options.days)
+UserPass = 'a2FtZW5vOkNTUzNDMzgw'   # Authorization Basic Base64
+SLT_URI  = 'https://asa.alma.cl/webslt/service/api/entries?'
+SLTstart = (datetime.datetime.today() - datetime.timedelta(days=backDays)).strftime('%Y-%m-%dT%H:%M:%S')
+queryText = 'curl -H\"Authorization: Basic %s\" \'%sintervalStart=%s&entryType=SBEX&schedBlockCode=%s&status=success\' > SLT.log' % (UserPass, SLT_URI, SLTstart, options.SBcode)
+os.system(queryText)
+fp = open('SLT.log', 'r')
+SLTline = fp.readlines()
+fp.close()
+SLTentries = SLTline[0].split('\"')
+for SLTentry in SLTentries:
+    if 'uid://A002/' in SLTentry : print('new %s' % (SLTentry))
