7e3da6d8377ebe6545c15986b672a87da9e9be91
Seiji.Kameno <seiji.kameno@nao.ac.jp>
2026-06-16 16:04:49 -0400

    modified:   SLTquery.py

--- SLTquery.py
+++ SLTquery.py
@@ -14,7 +14,7 @@ SLT_URI  = 'https://asa.alma.cl/webslt/service/api/entries?'
 SLTstart = (datetime.datetime.today() - datetime.timedelta(days=backDays)).strftime('%Y-%m-%dT%H:%M:%S')
 SLTend   = (datetime.datetime.today() - datetime.timedelta(hours=2)).strftime('%Y-%m-%dT%H:%M:%S')
 queryText = 'curl -H\"Authorization: Basic %s\" \'%sintervalStart=%s&intervalEnd=%s&entryType=SBEX&schedBlockCode=%s&status=success\' > SLT.log' % (UserPass, SLT_URI, SLTstart, SLTend, options.SBcode)
-print(queryText)
+#print(queryText)
 os.system(queryText)
 fp = open('SLT.log', 'r')
 SLTline = fp.readlines()
