2c68b39bd3ad3720981b082c5e05bb663d1fcd21
Seiji.Kameno <seiji.kameno@nao.ac.jp>
2026-09-02 13:45:52 -0400

    modified:   SLTquery.py

--- SLTquery.py
+++ SLTquery.py
@@ -4,9 +4,11 @@ import datetime
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option('-s', dest='SBcode', metavar='SBcode',
-    help='SB code  e.g. PSO_Grid', default='PSO_Grid')
+    help='SB code  e.g. PSO_Grid', default='')
 parser.add_option('-b', dest='Subject', metavar='Subject',
     help='Subject  e.g. baseline', default='')
+parser.add_option('-n', dest='ProjectName', metavar='ProjectName',
+    help='ProjectName  e.g. Per Source Grid Monitoring', default='')
 parser.add_option('-d', dest='days', metavar='days',
     help='Backword days', default='4')
 (options, args) = parser.parse_args()
@@ -18,17 +20,16 @@ if UserPass == '':
 SLT_URI  = 'https://asa.alma.cl/webslt/service/api/entries?'
 SLTstart = (datetime.datetime.today() - datetime.timedelta(days=backDays)).strftime('%Y-%m-%dT%H:%M:%S')
 SLTend   = (datetime.datetime.today() - datetime.timedelta(hours=2)).strftime('%Y-%m-%dT%H:%M:%S')
-#queryText = 'curl -H\"Authorization: Basic %s\" \'%sintervalStart=%s&intervalEnd=%s&entryType=SBEX&schedBlockCode=%s&status=success\' > SLT.log' % (UserPass, SLT_URI, SLTstart, SLTend, options.SBcode)
-if options.Subject == '':
-    queryText = 'curl -H\"Authorization: Basic %s\" \'%sintervalStart=%s&intervalEnd=%s&entryType=SBEX&schedBlockCode=%s&status=success\' > SLT.log' % (UserPass, SLT_URI, SLTstart, SLTend, options.SBcode)
-else:
-    queryText = 'curl -H\"Authorization: Basic %s\" \'%sintervalStart=%s&intervalEnd=%s&subject=%s&status=success\' > SLT.log' % (UserPass, SLT_URI, SLTstart, SLTend, options.Subject)
-#print(queryText)
+queryText = 'curl -H\"Authorization: Basic %s\" \'%sintervalStart=%s&intervalEnd=%s&status=success' % (UserPass, SLT_URI, SLTstart, SLTend)
+if options.SBcode  != '': queryText = queryText + '&schedBlockCode=' + options.SBcode
+if options.Subject != '': queryText = queryText + '&subject=' + options.Subject
+if options.ProjectName != '': queryText = queryText + '&projectName=' + options.ProjectName
+queryText = queryText + '\' > SLT.log'
+print(queryText)
 os.system(queryText)
 fp = open('SLT.log', 'r')
 SLTline = fp.readlines()
 fp.close()
-#if len(SLTline) > 0:
 SLTentries = SLTline[0].split('\"')
 UIDList = [SLTentries[index+2] for index, SLTentry in enumerate(SLTentries) if 'execBlockUid' in SLTentry]
 arrayNameList = [SLTentries[index+2] for index, SLTentry in enumerate(SLTentries) if 'arrayName' in SLTentry]
